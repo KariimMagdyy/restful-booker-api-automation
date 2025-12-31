d50e0be6a003ff17dc6b726e0db1854b85f945c9
KariimMagdyy <kmagdy74@gmail.com>
2026-01-01 00:42:50 +0200

    modify BaseTest

--- src/test/java/core/BaseTest.java
+++ src/test/java/core/BaseTest.java
@@ -1,19 +1,14 @@
 package core;
+
 import io.restassured.RestAssured;
 import io.restassured.parsing.Parser;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
+
     @BeforeClass
     public void setup() {
         RestAssured.defaultParser = Parser.JSON;
-
         TokenManager.resetToken();
-    }
-
-    @BeforeMethod
-    public void resetRestAssured() {
-        RestAssured.reset();
     }
 }
